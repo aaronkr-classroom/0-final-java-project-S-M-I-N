c75090bf763c16b82c800de4f199c0d5d9e96f4a
Seon-M <tjswo0428@naver.com>
2024-03-12 17:19:22 +0900

    Level 3 끝

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,23 +1,34 @@
+import java.util.Scanner;
 
 public class Welcome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("당신의 이름을 입력하세요: ");
+		String name = input.next();
+		
+		System.out.println("연락처를 입력하세요: ");
+		int phone = input.nextInt(); // 숫자만 입력
+		
 		
 	String greeting = "Welcome to Shopping Mall!";
 	String tagline = "Welcome to Book Market!!";
 	
-	System.out.println("*****************************************************");
+	System.out.println("************************************************");
 	System.out.println("\t" + greeting);
 	System.out.println("\t" + tagline);
-	System.out.println("*****************************************************");
+	System.out.println("************************************************");
 	
-	System.out.println("1. 고객 정보 확인하기 \t 5. 바구니에 항목 추가하기");
-	System.out.println("2. 장바구니 상품 목록 보기 \t 6. 장바구니의 항목 수량 줄이기");
-	System.out.println("3. 장바구니 비우기 \t 7. 장바구니의 항목 삭제하기");
-	System.out.println("4. 영수증 표시하기 \t 8. 종료");
-	System.out.println("*****************************************************");
-
+	System.out.println("1. 고객 정보 확인하기 \t5. 바구니에 항목 추가하기");
+	System.out.println("2. 장바구니 상품 목록 보기 \t6. 장바구니의 항목 수량 줄이기");
+	System.out.println("3. 장바구니 비우기 \t \t7. 장바구니의 항목 삭제하기");
+	System.out.println("4. 영수증 표시하기 \t \t8. 종료");
+	System.out.println("************************************************");
+	
+	System.out.println("메뉴 번호를 선택하세요: ");
+	int choice = input.nextInt(); // 숫자만 입력
+	System.out.println(choice + "번을 선택 했습니다.");
 	}
 
 }
